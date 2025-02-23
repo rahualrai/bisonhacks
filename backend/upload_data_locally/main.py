5a2ebced3215b43584fd7c2db0022d07cde55a65
Suprabhat Rijal <goodguysman@gmail.com>
2025-02-22 23:57:29 -0500

    Upload all data to local firebase

--- backend/upload_data_locally/main.py
+++ backend/upload_data_locally/main.py
@@ -1,9 +1,17 @@
 import json
 from firebase_admin import initialize_app, firestore
 import google.cloud.firestore
+from dotenv import load_dotenv
+from google import genai
+import os
+from google.cloud.firestore_v1.vector import Vector
 
 app = initialize_app()
 firestore_client: google.cloud.firestore.Client = firestore.client()
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 
 with open("./new_scholarships.json", "r") as file:
@@ -14,5 +22,11 @@ with open("./new_scholarships.json", "r") as file:
 i = 0
 for scholarship in scholarships:
     print(i)
+    desc = (
+        scholarship["description"] if len(scholarship["description"]) != 0 else "empty"
+    )
+    result = client.models.embed_content(model="text-embedding-004", contents=desc)
+    scholarship["description_embeddings"] = Vector(result.embeddings[0].values)
+
     doc_ref = firestore_client.collection("scholarships").add(scholarship)
     i += 1
